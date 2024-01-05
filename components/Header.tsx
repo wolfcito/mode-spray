67e9e168d7adb1d9e000bd5724fa5edd775c6fe3
Luis Fernando <guffenix@gmail.com>
2024-01-04 23:54:32 -0500

    chore: scaffold components

---
+++ components/Header.tsx
@@ -0,0 +1,96 @@
+import React, { useCallback, useRef, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import { useOutsideClick } from "~~/hooks/scaffold-eth";
+
+type HeaderMenuLink = {
+  label: string;
+  href: string;
+  icon?: React.ReactNode;
+};
+
+export const menuLinks: HeaderMenuLink[] = [
+  {
+    label: "Home",
+    href: "/",
+  },
+];
+
+export const HeaderMenuLinks = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      {menuLinks.map(({ label, href, icon }) => {
+        const isActive = router.pathname === href;
+        return (
+          <li key={href}>
+            <Link
+              href={href}
+              passHref
+              className={`${
+                isActive ? "bg-secondary shadow-md" : ""
+              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+            >
+              {icon}
+              <span>{label}</span>
+            </Link>
+          </li>
+        );
+      })}
+    </>
+  );
+};
+
+export function Header() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const burgerMenuRef = useRef<HTMLDivElement>(null);
+  useOutsideClick(
+    burgerMenuRef,
+    useCallback(() => setIsDrawerOpen(false), []),
+  );
+
+  return (
+    <div className="sticky top-0 z-20 justify-between flex-shrink-0 min-h-0 px-0 lg:static navbar sm:px-2">
+      <div className="w-auto navbar-start lg:w-1/2">
+        <div className="lg:hidden dropdown" ref={burgerMenuRef}>
+          <label
+            tabIndex={0}
+            className={`ml-1 btn btn-ghost ${isDrawerOpen ? "hover:bg-secondary" : "hover:bg-transparent"}`}
+            onClick={() => {
+              setIsDrawerOpen(prevIsOpenState => !prevIsOpenState);
+            }}
+          >
+            <Bars3Icon className="h-1/2" />
+          </label>
+          {isDrawerOpen && (
+            <ul
+              tabIndex={0}
+              className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+              onClick={() => {
+                setIsDrawerOpen(false);
+              }}
+            >
+              <HeaderMenuLinks />
+            </ul>
+          )}
+        </div>
+        <Link href="/" passHref className="items-center hidden gap-2 ml-4 mr-6 lg:flex shrink-0">
+          <div className="relative flex w-20 h-10">
+            <Image alt="logo" className="cursor-pointer" fill src="/logo.svg" />
+          </div>
+          <div className="flex flex-col">
+            <span className="text-2xl font-bold leading-tight">SPRAY</span>
+          </div>
+        </Link>
+      </div>
+      <div className="flex-grow mr-4 navbar-end">
+        <RainbowKitCustomConnectButton />
+        <FaucetButton />
+      </div>
+    </div>
+  );
+}
---
+++ components/MetaHeader.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Head from "next/head";
+
+type MetaHeaderProps = {
+  title?: string;
+  description?: string;
+  image?: string;
+  twitterCard?: string;
+  children?: React.ReactNode;
+};
+
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/` : "/";
+
+export const MetaHeader = ({
+  title = "Mode Spray",
+  description = "Spray tokens or ether to multiple address on the mode network",
+  image = "thumbnail.jpg",
+  twitterCard = "summary_large_image",
+  children,
+}: MetaHeaderProps) => {
+  const imageUrl = baseUrl + image;
+
+  return (
+    <Head>
+      {title && (
+        <>
+          <title>{title}</title>
+          <meta property="og:title" content={title} />
+          <meta name="twitter:title" content={title} />
+        </>
+      )}
+      {description && (
+        <>
+          <meta name="description" content={description} />
+          <meta property="og:description" content={description} />
+          <meta name="twitter:description" content={description} />
+        </>
+      )}
+      {image && (
+        <>
+          <meta property="og:image" content={imageUrl} />
+          <meta name="twitter:image" content={imageUrl} />
+        </>
+      )}
+      {twitterCard && <meta name="twitter:card" content={twitterCard} />}
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+      {children}
+    </Head>
+  );
+};
