2a1041525dc148f9ab9b57102d4ee15713bdc218
Luis Fernando <guffenix@gmail.com>
2024-01-14 11:28:20 -0500

    chore(impr): button link default style

--- components/button/button-link.componen.tsx
+++ components/button/button-link.componen.tsx
@@ -1,12 +1,15 @@
 import { ButtonProps } from './button.type'
+import clsx from 'clsx'
 
-export function ButtonLink({
-  onclick,
-  label = 'button',
-  className = 'text-xs text-right underline text-accent underline-offset-2 hover:text-neutral-content',
-}: ButtonProps) {
+export function ButtonLink({ onclick, label = 'button', className }: ButtonProps) {
   return (
-    <button className={className} onClick={onclick}>
+    <button
+      className={clsx(
+        'text-xs text-right underline text-accent underline-offset-2 hover:text-neutral-content',
+        className,
+      )}
+      onClick={onclick}
+    >
       {label}
     </button>
   )
