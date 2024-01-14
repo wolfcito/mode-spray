845fe3043601dbc60a898c19d2a7344baa0b5e74
Luis Fernando <guffenix@gmail.com>
2024-01-14 11:49:36 -0500

    chore: improve disperse flow and update useful validations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import { SetStateAction, useState } from 'react'
+import clsx from 'clsx'
 import { ethers } from 'ethers'
+import { useWalletClient } from 'wagmi'
 import { Button, ButtonLink } from '~~/components/button'
 import { MetaHeader } from '~~/components/header'
 import { getParsedError } from '~~/components/scaffold-eth'
+import { TnxLink } from '~~/components/txn-link'
 import { useScaffoldContractWrite } from '~~/hooks/scaffold-eth'
-import { notification } from '~~/utils/scaffold-eth'
+import { getBlockExplorerTxLink, notification } from '~~/utils/scaffold-eth'
 
 interface MyDictionaryProps {
   [key: string]: bigint
@@ -12,14 +15,20 @@ interface MyDictionaryProps {
 
 export default function Home() {
   const [confirmtnxs, setConfirmtnxs] = useState<boolean>(false)
+  const [isLoading, setLoading] = useState<boolean>(false)
+
   const [alltxns, setAlltxns] = useState<string>('')
   const [everyTxns, setEveryTxns] = useState<MyDictionaryProps>({})
   const [allAddress, setAllAddress] = useState<string[]>([])
   const [allValues, setAllValues] = useState<bigint[]>([])
 
   const [totalcost, setTotalcost] = useState(BigInt('0'))
+  const [infotxns, setInfotxns] = useState<`0x${string}` | undefined>(undefined)
+  const [blockhash, setblockhash] = useState<string>()
 
-  const { writeAsync, isLoading, isSuccess } = useScaffoldContractWrite({
+  const { data } = useWalletClient()
+
+  const { writeAsync } = useScaffoldContractWrite({
     contractName: 'ModeDisperse',
     functionName: 'disperseEther',
     args: [allAddress, allValues],
@@ -28,13 +37,14 @@ export default function Home() {
 
   const cleantxns = () => {
     setTotalcost(BigInt('0'))
-
+    setInfotxns(undefined)
     setAllAddress([])
     setAllValues([])
 
     setEveryTxns({})
     setAlltxns('')
     setConfirmtnxs(false)
+    setblockhash('')
   }
 
   const updateValue = (e: { target: { value: SetStateAction<string> } }) => {
@@ -47,6 +57,7 @@ export default function Home() {
     setAllAddress([])
     setAllValues([])
     setConfirmtnxs(false)
+    setblockhash('')
 
     try {
       const rows = contentfull.split('\n')
@@ -55,7 +66,7 @@ export default function Home() {
         const onerow = element.split(/[,\s;]+/)
         if (onerow[0].startsWith('0x') && onerow[0].length === 42) {
           const weiValue = ethers.parseEther(onerow[1])
-          allTxns[onerow[0]] = weiValue //BigInt(onerow[1])
+          allTxns[onerow[0]] = weiValue
         }
       })
 
@@ -82,14 +93,20 @@ export default function Home() {
   }
 
   const sprayEth = async () => {
+    setLoading(true)
     if (writeAsync) {
       try {
-        writeAsync()
-        if (isSuccess) cleantxns()
+        const data = await writeAsync()
+
+        if (!data) return
+        cleantxns()
+        setInfotxns(data)
+        getBlockexplorerTxnLink(data)
       } catch (e: any) {
         const message = getParsedError(e)
         notification.error(message)
-        setConfirmtnxs(false)
+      } finally {
+        setLoading(false)
       }
     }
   }
@@ -101,50 +118,63 @@ export default function Home() {
     contentValidation({ contentfull: text })
   }
 
+  const getBlockexplorerTxnLink = async (address: string) => {
+    if (!data) return
+    const network = await data.getChainId().then(chainId => chainId)
+    const linkTx = getBlockExplorerTxLink(network, address)
+    setblockhash(linkTx)
+  }
+
   return (
     <>
       <MetaHeader />
 
-      <div className="flex flex-col items-center flex-grow w-full xl:w-[626px] pt-10 self-center">
-        <div className="w-full p-5 md:border-t md:border-r border-t-secondary-content rounded-t-3xl bg-neutral">
-          <div className="flex flex-col space-y-3 py-7 rounded-3xl">
-            <ButtonLink onclick={pasteFromClipboard} label="Paste from Clipboard" />
+      <div className="flex flex-col items-center flex-grow w-full xl:w-[626px] self-center">
+        <div className="w-full p-2 rounded-t-3xl bg-black/20 backdrop-blur-sm">
+          <div className="flex flex-col space-y-3 pb-7 rounded-3xl">
+            <ButtonLink onclick={pasteFromClipboard} label="Paste from Clipboard" className="text-neutral-content" />
 
             <textarea
               value={alltxns}
               onChange={updateValue}
-              placeholder="0x6a22F6308a9a8D40eb7585F16BBd73913cF98633 100000000000000"
-              className="w-full rounded-lg textarea textarea-bordered textarea-primary textarea-sm min-h-[135px]"
+              placeholder="0x6a22F6308a9a8D40eb7585F16BBd73913cF98633 0.001"
+              className="w-full rounded-lg textarea textarea-bordered textarea-primary textarea-xs min-h-[135px] font-mono"
             ></textarea>
             <ButtonLink onclick={cleantxns} label="clear" />
           </div>
         </div>
 
+        <TnxLink infotxns={infotxns} blockhash={blockhash} />
+
         {confirmtnxs ? (
-          <div className="flex flex-col w-full px-5 pb-10 bg-neutral">
-            <p>Confirm your transactions</p>
-            <table className="table table-zebra">
-              <thead>
-                <tr>
-                  <th>txn</th>
-                  <th>address</th>
-                  <th className="text-right">amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Object.entries(everyTxns).map(({ '0': address, '1': amount }, index) => (
-                  <tr key={`${address}-row`}>
-                    <th>{index + 1}</th>
-                    <td>{address}</td>
-                    <td className="text-right">{`${ethers.formatEther(amount)} ETH`}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <div className="pt-2 pb-3 pr-4 text-sm font-semibold text-right border-t border-secondary text-neutral-content">
-              {`Total amount: ${ethers.formatEther(totalcost)} ETH`}
+          <div className="flex flex-col w-full bg-[url('/mode/mode-hand.png')] bg-right-bottom bg-contain bg-no-repeat">
+            <div className="flex flex-col px-2 pb-10 text-xs bg-black/40 backdrop-blur-sm">
+              <p className="font-bold">Confirm your transactions</p>
+              <div className="overflow-x-auto">
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead>
+                    <tr>
+                      <th>txn</th>
+                      <th>address</th>
+                      <th className="text-right">amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {Object.entries(everyTxns).map(({ '0': address, '1': amount }, index) => (
+                      <tr key={`${address}-row`}>
+                        <td className="p-2">{index + 1}</td>
+                        <td className="font-mono">{address}</td>
+                        <td className="font-mono text-right">{`${ethers.formatEther(amount)} ETH`}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div className="pt-2 pb-3 pr-4 font-mono text-sm font-semibold text-right border-t border-primary-content text-neutral-content">
+                {`Total amount: ${ethers.formatEther(totalcost)} ETH`}
+              </div>
+              <Button onclick={sprayEth} label="Spray" className={clsx('self-center my-5 w-36')} disabled={isLoading} />
             </div>
-            <Button onclick={sprayEth} label="Spray" className="self-center my-5 w-36" disabled={isLoading} />
           </div>
         ) : null}
       </div>
