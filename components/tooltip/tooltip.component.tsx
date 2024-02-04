e30aafd1a4d7cfed3d1e75c532c727c09538386a
Luis Fernando <guffenix@gmail.com>
2024-02-03 23:45:55 -0500

    style(impr): sizing on tooltip

--- components/tooltip/tooltip.component.tsx
+++ components/tooltip/tooltip.component.tsx
@@ -5,15 +5,15 @@ import clsx from 'clsx'
 export function Tooltip({ className, toolTipText }: tooltipProps) {
   return (
     <div className="inline-block">
-      <span className="group relative inline-block text-blue-500 underline hover:text-red-500 duration-300">
-        <Icon className="text-base opacity-50 hover:opacity-80 transition ease-out cursor-pointer duration-300" />
+      <span className="relative inline-block text-blue-500 underline duration-300 group hover:text-red-500">
+        <Icon className="text-base transition duration-300 ease-out opacity-50 cursor-pointer hover:opacity-80" />
         <span
           className={clsx(
-            "absolute hidden group-hover:flex -left-5 -top-2 -translate-y-full w-48 px-3 py-4 bg-neutral-900 text-left text-[#888] text-xs after:content-[''] after:absolute after:left-7 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-neutral-900",
+            "absolute hidden group-hover:flex -left-5 min-w-[192px] -top-2 -translate-y-full w-auto px-3 py-4 bg-neutral-900 text-left text-[#888] text-xs after:content-[''] after:absolute after:left-7 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-neutral-900",
             className,
           )}
         >
-          {toolTipText}
+          <span className="break-words">{toolTipText}</span>
         </span>
       </span>
     </div>
