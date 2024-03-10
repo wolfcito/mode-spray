01c351a49f33fb979f96f565b78841e7408e944c
Luis Fernando <guffenix@gmail.com>
2024-03-10 18:13:20 -0500

    chore: reorganizing core types

--- types/mode-spray.d.ts
+++ types/mode-spray.d.ts
@@ -1,1 +1,11 @@
 export type AddressProp = `0x${string}`
+
+export interface SprayTransactionProps {
+  key: string
+  value: bigint
+}
+
+export interface TokenSelectedProps {
+  tokenAddress: AddressProp
+  tokenSymbol: string
+}
