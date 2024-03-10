d7f19b578468fa2c003643c64ae40a5b2c4aa9c7
Luis Fernando <guffenix@gmail.com>
2024-03-09 21:06:50 -0500

    style: improve footer elements

--- components/footer/footer.component.tsx
+++ components/footer/footer.component.tsx
@@ -4,13 +4,13 @@ import { colophon } from '~~/constants'
 
 export function Footer() {
   return (
-    <footer className="bg-black text-white py-4 px-6 h-16 flex justify-between items-center fixed bottom-0 left-0 w-full">
+    <footer className="fixed bottom-0 left-0 flex items-center justify-between w-full h-16 px-6 py-4 text-white bg-black">
       <div className="flex space-x-2">
         {colophon.map((author, index) => (
           <a
             href={author.link}
             key={index}
-            className="hover:text-gray-400 text-[#4B4F52] px-3 py-1 rounded-full border border-[#565A5E]"
+            className="hover:text-gray-400 text-[#4B4F52] px-3 py-1 rounded-sm border border-[#565A5E]"
           >
             {author.name}
           </a>
