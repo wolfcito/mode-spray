fc74510a55e2257b447e07e5b9d908344a4ec40c
Luis Fernando <guffenix@gmail.com>
2024-03-17 23:47:02 -0500

    chore(impr): balance total style updated

--- components/scaffold-eth/Balance.tsx
+++ components/scaffold-eth/Balance.tsx
@@ -16,10 +16,10 @@ export const Balance = ({ address, className = '' }: BalanceProps) => {
 
   if (!address || isLoading || balance === null) {
     return (
-      <div className="animate-pulse flex space-x-4">
-        <div className="rounded-md bg-slate-300 h-6 w-6"></div>
+      <div className="flex space-x-4 animate-pulse">
+        <div className="w-6 h-6 rounded-md bg-slate-300"></div>
         <div className="flex items-center space-y-6">
-          <div className="h-2 w-28 bg-slate-300 rounded"></div>
+          <div className="h-2 rounded w-28 bg-slate-300"></div>
         </div>
       </div>
     )
@@ -35,10 +35,10 @@ export const Balance = ({ address, className = '' }: BalanceProps) => {
 
   return (
     <button
-      className={`btn btn-sm btn-ghost flex flex-col font-normal items-center hover:bg-transparent ${className}`}
+      className={`btn btn-sm btn-ghost flex flex-col font-mono items-center hover:bg-transparent ${className}`}
       onClick={onToggleBalance}
     >
-      <div className="w-full flex items-center justify-center">
+      <div className="flex items-center justify-center w-full">
         {isEthBalance ? (
           <>
             <span>{balance?.toFixed(4)}</span>
