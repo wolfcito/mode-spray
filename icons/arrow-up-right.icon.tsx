e2ab53156b031321c01964b0b6561df438a5390e
Luis Fernando <guffenix@gmail.com>
2024-03-17 23:54:37 -0500

    feat: icons for arrow up right, bell, mode, prush protocol icons added

---
+++ icons/arrow-up-right.icon.tsx
@@ -0,0 +1,7 @@
+export function ArrowUpRightIcon() {
+  return (
+    <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+    </svg>
+  )
+}
---
+++ icons/index.ts
@@ -0,0 +1,4 @@
+export * from './bell.icon'
+export * from './push-logo.icon'
+export * from './mode.icon'
+export * from './arrow-up-right.icon'
