e5b5ec2c14e44d29fcefc0988810dece449c69de
Luis Fernando <guffenix@gmail.com>
2024-03-18 23:22:32 -0500

    chore(impr): update ui network component

--- components/wrong-network/wrong-network.component.tsx
+++ components/wrong-network/wrong-network.component.tsx
@@ -10,7 +10,7 @@ export function WrongNetwork() {
   return (
     <>
       <MetaHeader />
-      <div className="flex flex-col items-center w-full xl:w-[626px] self-center bg-black border border-[#ADB5BD] mx-4 mt-10 md:mt-10 px-4 md:px-8  py-10">
+      <div className="flex flex-col items-center w-full xl:w-[626px] self-center bg-black border border-[#ADB5BD] mx-4 mt-10 md:mt-10 px-4 md:px-8  py-10 rounded-sm">
         <div className="w-full p-2 rounded-t-3xl bg-black/20 backdrop-blur-sm">
           <div className="flex flex-col pb-2 space-y-3 rounded-3xl">
             <p className="text-3xl font-semibold font-chakra">Wrong Network</p>
