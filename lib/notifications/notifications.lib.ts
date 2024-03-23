57846bdc5696eedf55498704b7d8fc5e8ef746d3
Luis Fernando <guffenix@gmail.com>
2024-03-22 23:29:21 -0500

    chore(impr): get singer method for specific channel

--- lib/notifications/notifications.lib.ts
+++ lib/notifications/notifications.lib.ts
@@ -1,11 +1,10 @@
 import { Wallet } from 'ethers'
 import { logger } from '~~/lib/logger'
 
-export const getSigner = (DELEGATE_KEY: string) => {
-  console.log('DELEGATE_KEY', DELEGATE_KEY)
-  if (!DELEGATE_KEY) {
-    logger.error('DELEGATE_KEY env var is invalid or missing. You can either add a valid one')
+export const getSigner = (delegate: string) => {
+  if (!delegate) {
+    logger.error('delegate env var is invalid or missing. You can either add a valid one')
     return undefined
   }
-  return new Wallet(`0x${DELEGATE_KEY}`)
+  return new Wallet(`0x${delegate}`)
 }
