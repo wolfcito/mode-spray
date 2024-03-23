88e50ac903d3c7b5180a46d80c2670daacfce695
Luis Fernando <guffenix@gmail.com>
2024-03-22 23:39:24 -0500

    feat: enable use of push protocol detailed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from 'react'
+import { GetStaticProps } from 'next'
 import { useNetwork } from 'wagmi'
 import { MetaHeader } from '~~/components/header'
 import { SprayMenuSelector } from '~~/components/spray-menu-selector'
 import { WrongNetwork } from '~~/components/wrong-network'
 import { useTargetNetwork } from '~~/hooks/scaffold-eth/useTargetNetwork'
 import { logger } from '~~/lib'
+import { useGlobalState } from '~~/services/store/store'
 import { TokenSelectedProps } from '~~/types/mode-spray'
 import { SprayETH, SprayToken } from '~~/view'
 
-export default function Home() {
+export default function Home({ delegateNotificator }: HomeProps) {
+  const setDelegate = useGlobalState(({ setDelegate }) => setDelegate)
   const [isCustom, setIsCustom] = useState<boolean>(false)
   const [tokenSelected, setTokenSelected] = useState({} as TokenSelectedProps)
 
   const { targetNetwork } = useTargetNetwork()
   const { chain } = useNetwork()
+
+  if (delegateNotificator) setDelegate(delegateNotificator)
 
   useEffect(() => {
     setIsCustom(false)
@@ -31,7 +36,7 @@ export default function Home() {
   return (
     <>
       <MetaHeader />
-      <div className="flex flex-col items-center w-full xl:w-[626px] self-center bg-black border border-[#ADB5BD] mx-4 mt-20 md:mt-10 px-4 md:px-8  py-10">
+      <div className="flex flex-col items-center w-full xl:w-[626px] self-center bg-black border border-[#ADB5BD] mx-4 mt-10 md:mt-10 px-4 md:px-8  py-10 rounded-sm">
         <div className="w-full p-2 rounded-t-3xl bg-black/20 backdrop-blur-sm">
           <div className="flex flex-col pb-2 space-y-3 rounded-3xl">
             <SprayMenuSelector
@@ -48,4 +53,15 @@ export default function Home() {
       </div>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const delegateNotificator = process.env.DELEGATE_NOTIFICATOR ?? 'disabled'
+  return {
+    props: { delegateNotificator },
+  }
+}
+
+interface HomeProps {
+  readonly delegateNotificator: string
 }
