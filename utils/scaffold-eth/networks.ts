ef3813c9fdc58f24825301c7be166bff969fe642
Luis Fernando <guffenix@gmail.com>
2024-04-10 23:24:59 -0500

    feat: get ETH amount by tx

--- utils/scaffold-eth/networks.ts
+++ utils/scaffold-eth/networks.ts
@@ -136,3 +136,29 @@ export function getTokenAmountByTxn(chainId: number, txnHash: string) {
 
   return `${blockExplorerTxURL}/api/v2/transactions/${txnHash}/token-transfers?type=ERC-20`
 }
+
+/**
+ * Get the ETH amount by txn hash
+ */
+export function getETHAmountByTxn(chainId: number, txnHash: string) {
+  const chainNames = Object.keys(chains)
+
+  const targetChainArr = chainNames.filter(chainName => {
+    const wagmiChain = chains[chainName as keyof typeof chains]
+    return wagmiChain.id === chainId
+  })
+
+  if (targetChainArr.length === 0) {
+    return ''
+  }
+
+  const targetChain = targetChainArr[0] as keyof typeof chains
+  // @ts-expect-error : ignoring error since `blockExplorers` key may or may not be present on some chains
+  const blockExplorerTxURL = chains[targetChain]?.blockExplorers?.default?.url
+
+  if (!blockExplorerTxURL) {
+    return ''
+  }
+
+  return `${blockExplorerTxURL}/api/v2/transactions/${txnHash}`
+}
