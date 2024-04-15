3c941eb8028d87555841135914fccbd705eb2685
Luis Fernando <guffenix@gmail.com>
2024-04-14 23:28:39 -0500

    feat: enable support on Base Network

--- scaffold.config.ts
+++ scaffold.config.ts
@@ -12,7 +12,7 @@ export type ScaffoldConfig = {
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.modeTestnet, modeMainnet],
+  targetNetworks: [chains.modeTestnet, modeMainnet, chains.baseSepolia, chains.base],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
