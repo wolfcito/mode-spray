ffdbba60fb80ea7ab5ef55934f34e6c11dc34066
Luis Fernando <guffenix@gmail.com>
2024-04-21 22:38:34 -0500

    refactor: header button wrapper

--- components/button-wrapper/button-wrapper.component.tsx
+++ components/button-wrapper/button-wrapper.component.tsx
@@ -1,17 +1,19 @@
 import { ButtonWrapperProps } from './button-wrapper.type'
 import clsx from 'clsx'
 
-export function ButtonWrapper({ children, classname }: ButtonWrapperProps) {
+export function ButtonWrapper({ children, classname, onclick }: ButtonWrapperProps) {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const noop = () => {}
   return (
-    <>
-      <div
-        className={clsx(
-          'items-center hidden gap-2 px-4 py-3 rounded-sm lg:flex hover:rounded-sm hover:bg-neutral cursor-pointer',
-          classname,
-        )}
-      >
-        {children}
-      </div>
-    </>
+    <div
+      className={clsx(
+        'items-center hidden gap-2 px-4 py-3 rounded-sm lg:flex hover:rounded-sm hover:bg-neutral cursor-pointer',
+        classname,
+      )}
+      onClick={onclick ? onclick : noop}
+      role="button"
+    >
+      {children}
+    </div>
   )
 }
