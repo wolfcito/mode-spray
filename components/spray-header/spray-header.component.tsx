f0b03b486c209148c848adfe9ba02841cc98ab35
Luis Fernando <guffenix@gmail.com>
2024-04-21 22:39:29 -0500

    refactor: cleanup

--- components/spray-header/spray-header.component.tsx
+++ components/spray-header/spray-header.component.tsx
@@ -3,15 +3,13 @@ import { Tooltip } from '~~/components/tooltip'
 
 export function SprayHeader({ tokenName }: SprayHeaderProps) {
   return (
-    <div className="flex items-end justify-between">
-      <div>
-        <h2 className="p-0 m-0 text-2xl font-semibold font-chakra">
-          Account Panel <Tooltip toolTipText={`Allowed separators  , ; = & (space)`} />
-        </h2>
-        <p className="font-mono text-xs font-normal m-0 text-[#4c4c4c]">
-          one address and amount[{tokenName}] on each line
-        </p>
-      </div>
+    <div className="flex flex-col">
+      <h2 className="p-0 m-0 text-2xl font-semibold font-chakra">
+        Account Panel <Tooltip toolTipText={`Allowed separators  , ; = & (space)`} />
+      </h2>
+      <p className="font-mono text-xs font-normal m-0 text-[#4c4c4c]">
+        {'one address and amount '} <span className="text-neutral-content">[{tokenName}]</span> {' on each line'}
+      </p>
     </div>
   )
 }
