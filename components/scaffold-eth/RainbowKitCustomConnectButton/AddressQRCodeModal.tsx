2e6e5b807b9cebfa28ea9e689546d6f18950022c
Luis Fernando <guffenix@gmail.com>
2024-04-21 22:36:59 -0500

    chore: improve ui consistency

--- components/scaffold-eth/RainbowKitCustomConnectButton/AddressQRCodeModal.tsx
+++ components/scaffold-eth/RainbowKitCustomConnectButton/AddressQRCodeModal.tsx
@@ -9,25 +9,23 @@ type AddressQRCodeModalProps = {
 
 export const AddressQRCodeModal = ({ address, modalId }: AddressQRCodeModalProps) => {
   return (
-    <>
-      <div>
-        <input type="checkbox" id={`${modalId}`} className="modal-toggle" />
-        <label htmlFor={`${modalId}`} className="modal cursor-pointer">
-          <label className="modal-box relative">
-            {/* dummy input to capture event onclick on modal box */}
-            <input className="h-0 w-0 absolute top-0 left-0" />
-            <label htmlFor={`${modalId}`} className="btn btn-ghost btn-sm btn-circle absolute right-3 top-3">
-              ✕
-            </label>
-            <div className="space-y-3 py-6">
-              <div className="flex space-x-4 flex-col items-center gap-6">
-                <QRCodeSVG value={address} size={256} />
-                <Address address={address} format="long" disableAddressLink />
-              </div>
-            </div>
+    <div>
+      <input type="checkbox" id={`${modalId}`} className="modal-toggle" />
+      <label htmlFor={`${modalId}`} className="cursor-pointer modal">
+        <label className="relative rounded-sm modal-box">
+          {/* dummy input to capture event onclick on modal box */}
+          <input className="absolute top-0 left-0 w-0 h-0" />
+          <label htmlFor={`${modalId}`} className="absolute rounded-sm btn btn-ghost btn-sm btn-circle right-3 top-3">
+            ✕
           </label>
+          <div className="py-6 space-y-3">
+            <div className="flex flex-col items-center gap-6 space-x-4">
+              <QRCodeSVG value={address} size={256} />
+              <Address address={address} format="long" disableAddressLink />
+            </div>
+          </div>
         </label>
-      </div>
-    </>
+      </label>
+    </div>
   )
 }
