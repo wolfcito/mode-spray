4c9f3585cc8d126d7514448652c8e76df39e2f0a
Luis Fernando <guffenix@gmail.com>
2024-04-28 23:14:00 -0500

    chore(impr): notification panel ui consistency

--- components/notifications/tab-notification.component.tsx
+++ components/notifications/tab-notification.component.tsx
@@ -2,21 +2,31 @@ import Link from 'next/link'
 import { NotificationByAddress } from './notifications.type'
 import { nanoid } from 'nanoid'
 import { useWalletClient } from 'wagmi'
-import { ArrowUpRightIcon } from '@heroicons/react/24/outline'
+import { ArrowUpRightIcon, EyeIcon } from '@heroicons/react/24/outline'
 
 export function TabNotification({ notificationList = [] }: { notificationList: NotificationByAddress[] }) {
-  const datosClient = useWalletClient()
+  const { data: dataClient } = useWalletClient()
 
   if (!notificationList.length) return <div className="flex flex-col w-full gap-4">{`You haven't notifications`}</div>
 
   return (
     <li>
+      <Link
+        key={nanoid()}
+        className="flex my-1.5 rounded-sm hover:rounded-sm text-neutral-content hover:skeleton"
+        href={`${dataClient?.chain.rpcUrls.default}/address/${dataClient?.account.address}?tab=token_transfers`}
+        target="_blank"
+      >
+        <EyeIcon className="w-4 h-6 ml-2 sm:ml-0" />
+        <div className="flex flex-col flex-1">View ON-CHAIN transactions</div>
+        <ArrowUpRightIcon className="w-4 h-6 ml-2 sm:ml-0" />
+      </Link>
       {notificationList.map(notifyment => {
         return (
           <Link
             key={nanoid()}
-            className="flex my-1.5 rounded-sm bg-neutral hover:rounded-sm hover:text-neutral-content hover:skeleton"
-            href={`https://sepolia.explorer.mode.network/address/${datosClient.data?.account.address}?tab=token_transfers`}
+            className="flex my-1.5 rounded-sm bg-neutral/50 hover:rounded-sm hover:text-neutral-content hover:skeleton"
+            href={notifyment.cta}
             target="_blank"
           >
             <div className="flex flex-col flex-1">
@@ -26,7 +36,6 @@ export function TabNotification({ notificationList = [] }: { notificationList: N
                 {notifyment.notification.body}
               </div>
             </div>
-            <ArrowUpRightIcon className="w-4 h-6 ml-2 sm:ml-0" />
           </Link>
         )
       })}
