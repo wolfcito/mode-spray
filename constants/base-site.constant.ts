1727cd8aa5b0e515c71ebdd0b46eab55e2c488a3
Luis Fernando <guffenix@gmail.com>
2024-07-04 23:39:04 -0500

    fix: correct linking

--- constants/base-site.constant.ts
+++ constants/base-site.constant.ts
@@ -1,17 +1,19 @@
 import { enviroment } from '~~/config'
 
+// used on chain icons
 export const baseSite = enviroment.debug
   ? 'http://localhost:3000'
   : enviroment.lab
   ? 'https://lab.modespray.xyz'
-  : 'https://modespray.xyz'
+  : 'https://app.modespray.xyz'
 
 export const labelSite = enviroment.lab ? 'Spray Laboratory' : 'Spray Production'
 
+// switch envs
 export const baseSiteLink = enviroment.debug
   ? 'http://localhost:3000'
   : enviroment.lab
-  ? 'https://modespray.xyz'
+  ? 'https://app.modespray.xyz'
   : 'https://lab.modespray.xyz'
 
 export const labeSiteLink = enviroment.lab ? 'Spray Production' : 'Spray Laboratory'
