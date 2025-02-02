2a4b0786ec11b132be3a0884d68fbeb7be5a7614
Luis Fernando <guffenix@gmail.com>
2025-02-02 01:12:28 -0500

    chore: update config

--- next.config.js
+++ next.config.js
@@ -2,23 +2,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors 'none';",
-          },
-        ],
-      },
-    ]
-  },
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === 'true',
